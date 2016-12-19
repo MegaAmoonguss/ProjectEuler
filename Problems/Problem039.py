3818b29e0291d5bc839b28874b732812cc2fd467
Graham Preston <graham.preston@gmail.com>
2016-12-19 10:43:12 -0500

    Removed prints from day 12, edits to day 19 (unfinished with code)

---
+++ Problems/Problem039.py
@@ -0,0 +1,32 @@
+def main():
+    max_num = 120
+    max_tri = 3
+     
+    for n in range(121, 1001):
+        print(n)
+        triangles = num_tri(n)
+        if triangles > max_tri:
+            max_tri = triangles
+            max_num = n
+    print(max_num)
+        
+def num_tri(p):
+    count = 0
+    triples = []
+    for i in range(1, int(p / 2)):
+        for j in range(i, int(p / 2)):
+            k = p - i - j
+            sides = sorted((i, j, k))
+            if sides[0]**2 + sides[1]**2 == sides[2]**2 and not contains(sides, triples):
+                count += 1
+                triples.append(sides)
+    return count
+
+def contains(e, l):
+    for i in l:
+        if i == e:
+            return True
+    return False
+    
+if __name__ == '__main__':
+    main()
