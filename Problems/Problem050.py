6013221c223bdc57e0d348c86aed112e0fdc4714
Graham Preston <graham.preston@gmail.com>
2016-12-22 15:12:24 -0500

    Problem 50

--- Problems/Problem050.py
+++ Problems/Problem050.py
@@ -1,16 +1,22 @@
-from euler import is_prime
-from euler import prime_list
+from euler import is_prime, prime_list
 
-sum = 0
+primes = prime_list(200000)
+print('Primes calculated.')
+
 max = 0
-primes = prime_list(1000)
-for p in primes:
-    if sum + p >= 1000:
-        break
-    sum += p
-    if is_prime(sum):
-        max = sum
+num_terms = 0
+for j in range(len(primes)):
+    for i in range(len(primes)):
+        if i + j == len(primes):
+            break
+        total = sum(primes[i:i+j])
+        if total >= 1000000:
+            break
+        
+        if total % 2 == 1 and is_prime(total):
+            num_terms = j
+            max = total
+            max_range = primes[i:i+j]
 
-
-
-print(max)
+print(num_terms, 'terms')
+print(max, 'total')
